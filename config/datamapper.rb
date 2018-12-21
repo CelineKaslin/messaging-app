2f2b5cb94ed1214c71fab5404397366b86976cd8
celine kaslin <celine.kaslin@gmail.com>
2018-12-21 15:37:49 +0000

    switch between database

--- config/datamapper.rb
+++ config/datamapper.rb
@@ -1,6 +1,9 @@
 require './lib/message'
 
-DataMapper.setup(:default, "postgres://localhost/messenger/#{ENV["RACK_ENV"]}")
+DataMapper.setup(:default, "postgres://localhost/messenger_#{ENV["RACK_ENV"]}")
 
 DataMapper.finalize
-DataMapper.auto_migrate!
+
+DataMapper.auto_migrate! if ENV["RACK_ENV"] == "test"
+
+DataMapper.auto_upgrade! if ENV["RACK_ENV"] == "development"
